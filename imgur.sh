96bc716af86b584f49faad07a9b2e6c35a2a6e81
manabutameni <M8R-loaggn@mailinator.com>
2012-07-12 19:53:00 -0500

    Changed some coding practices.

--- imgur.sh
+++ imgur.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 gallery_url=$1
-http_proxy=""
+tempname=`basename $0`
+tempfile=`mktemp -t ${tempname}` || exit 1
 
-if [ -n "$1" ]  #  If command-line argument present,
+if [ -n "$gallery_url" ]  #  If command-line argument present,
 then
 
-  if [[ "$1" =~ "imgur.com" ]]
+  if [[ "$gallery_url" =~ "imgur.com" ]]
   then
 
     i=0
-    curl -s $1 > temp.html
-    album_title=`cat temp.html | grep "h1 style"`
+    curl -s $gallery_url > $tempfile
+    album_title=`grep "h1 style" $tempfile`
 
     if [[ ${#album_title} == 18 ]]
     then
-      album_title=${1:(-5)}
+      album_title=${gallery_url:(-5)}
     else
       album_title=$(echo $album_title | sed -e 's/<[^>][^>]*>//g' -e '/^ *$/d')
     fi
 
     mkdir -p $album_title
-    mv temp.html $album_title
-    cd $album_title
-    for image in $(cat temp.html | awk -F\" '/data-src/ { print $10 }')
+    for image in $(awk -F\" '/data-src/ { print $10 }' $tempfile)
     do
       let i=$i+1;
-      curl $image > $i.jpg
+      curl $image > $album_title/$i.jpg
     done
 
   else
     echo -e "\nImgur albums only\n"
-    exit
+    exit 1
   fi
 
-  rm temp.html
+  rm $tempfile
 
 else
   echo -e "\nYou need to enter a parameter. Such as \
     \nhttps://www.imgur.com/a/qwerty\n"
+  exit 1
 fi
