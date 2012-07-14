17e1d988402435ee3635f59c2efd581b9b1c1314
manabutameni <M8R-loaggn@mailinator.com>
2012-07-13 20:47:16 -0500

    Attrib. and license

--- imgur.sh
+++ imgur.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 #=======================================#
-# All rights reserved.                  #
-# Author's github.                      #
 # https://github.com/manabutameni/Imgur #
 #=======================================#
 
