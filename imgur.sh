502636ed55cc031c7874bd15461762e7379025e4
manabutameni <tundh@hotmail.com>
2013-01-11 13:31:49 -0600

    major update
    Added debug flag
    Removed multiple album flag
    Changed the way you add multiple URLs (./imgur.sh url [url])
    Fixed bugs with getopts while loop
    Allowed for full word option settings (./imgur.sh --silent)
    Changed the way gallery_url is populated, fixing multiple bugs
    moved the parsing of folder names to a function.
    formatting.

--- imgur.sh
+++ imgur.sh
@@ -15,32 +15,38 @@ multiple_urls="FALSE"
 sanitize="FALSE"
 preserve="FALSE"
 silent_flag="FALSE"
+debug_flag="FALSE"
 curl_args="-s"
 data_index=-1
 count=0
 gallery_url=('') 
 
 # Functions
-long_desc()
+function debug()
 {
-  cat | less << EOF
+  if [[ "$debug_flag" == "TRUE" ]] && [[ "$silent_flag" == "FALSE" ]]
+  then
+    echo DEBUG: $*
+  fi
+}
+
+function long_desc()
+{
+  cat << EOF
 NAME
     imgur - a simple album downloader
 
 SYNOPSIS
-    Download albums from http://imgur.com while retaining order.
+    Download albums from imgur.com while retaining order.
 
 OPTIONS
     -h        Show this message.
-    -m <File> Download multiple albums found in <File>.
     -c        Clean nonalphanumeric characters from the album's name.
     -p        Preserve imgur's naming. (Warning! This will not retain order.)
-    -s        Silent mode.
+    -s        Silent mode. Overrides debug mode.
+    -d        Debug mode.
 
 EXAMPLES
-    bash imgur.bash [-cps] URL
-    bash imgur.bash -m FILE
-
     bash imgur.bash http://imgur.com/a/fG58m#0
     bash imgur.bash reactiongifsarchive.imgur.com
 
@@ -49,14 +55,35 @@ AUTHOR
     https://github.com/manabutameni/Imgur
 
 EOF
+exit 0
 }
 
-short_desc()
+function short_desc()
 {
   cat << EOF
-usage: $0 [-cps] URL
-usage: $0 [-m file]
+usage: $0 [-cps] URL [URL]
 EOF
+exit 1
+}
+
+function parse_folder_name()
+{
+  temp_folder_name="$(awk -F\" '/data-title/ {print $6; exit}' $htmltemp)"
+
+  if [[ "$sanitize" == "TRUE" ]]
+  then # remove special characters
+    clean=${temp_folder_name//_/} #turn / into _
+    clean=${clean// /_} #turn spaces into _
+    temp_folder_name="${clean//[^a-zA-Z0-9_]/}" # remove all special chars
+  else
+    temp_folder_name=$(sed 's/\//_/g' <<< $temp_folder_name) # ensure no / chars
+  fi
+
+  if [[ "$preserve" == "TRUE" ]] || [ -z "$temp_folder_name" ]
+  then # Create a name for a folder name based on the URL.
+    temp_folder_name=$(basename "$url" | sed 's/\#.*//g')
+  fi
+  echo $temp_folder_name
 }
 
 function float_eval()
@@ -76,103 +103,92 @@ function progress_bar()
   fi
 }
 
-# Program begins
-while getopts "hm:cps:" OPTION
+# Option Parsing
+while getopts ":dhcps" OPTION
 do
   case $OPTION in
-    h)
+    d | debug)
+      # print debugging messages
+      debug_flag="TRUE"
+      ;;
+    h | help)
       long_desc
-      exit 0
       ;;
-    m)
-      # This will output all imgur ablum urls from plain text <File> into the
-      # array $gallery_url. Note: urls must be separated by newline.
-      multiple_urls="TRUE"
-      gallery_url=( $(cat "$OPTARG") )
-      ;;
-    c)
+    c | clean)
       # Clean non alpha-numeric characters from album name.
-      # Useful for the (rare) albums named !!!OMG$)(@@*$@$%@
+      # Useful for the (rare) albums named !!!$)(@@*$@$%@
+      debug Clean Flag Set
       sanitize="TRUE"
       ;;
-    p)
+    p | preserve)
       # Preserve Imgur's naming scheme. Please note that this will not keep the
       # order of the images. While this does break the spirit of the script it
       # is included here for the sake of completion.
+      debug Preserve Flag Set
       preserve="TRUE"
       ;;
-    s)
+    s | slient)
       # Run silently.
+      debug Silent Flag Set
       curl_args="-s"
       silent_flag="TRUE"
       ;;
-    :)
+    '?' | *)
+      echo "Invalid option: -$OPTARG" >&2
       short_desc
-      exit 0
       ;;
-    ?)
-      short_desc
-      exit 1
   esac
 done
+shift $((OPTIND - 1))
 
-if [[ "$multiple_urls" == "FALSE" ]]
-then # set gallery_url to last argument if not downloading multiple albums.
-  gallery_url[0]="${@: -1}"
+gallery_url=("$@")
+
+if [[ $debug_flag == "TRUE" ]]
+then
+  for (( i = 0 ; i < ${#@} ; i++ ))
+  do
+    debug gallery_url[$i] '=' ${gallery_url[$i]}
+  done
 fi
 
 # make sure gallery_url isn't empty.
 if [[ -z ${gallery_url[0]} ]]
 then
+  debug '$gallery_url[0] is empty'
   short_desc
   exit 1
 fi
 
-if [[ "$gallery_url[0]" =~ ".imgur.com" ]]
-then
-  curl -s "${gallery_url[0]}" > $htmltemp
-
-  # Downloading html file for recursively searching for multiple gallery URLs.
-  # Allowing for complete downloads of albums inside albums.
-  # This will need to be updated when Imgur hits six character album URLs
-  gallery_url=( $(grep -oh "imgur.com/a/[a-zA-Z0-9]\{5\}" $htmltemp) )
-fi
-
+# Program Begins
+# This will need to be updated when Imgur hits six character album URLs
+# gallery_url=( $(grep -oh "imgur.com/a/[a-zA-Z0-9]\{5\}" $htmltemp) )
 for url in ${gallery_url[@]}
 do
+  debug '$url = ' $url
+  count=0 # Reset counter
   if [[ "$url" =~ "imgur.com/a/" ]]
   then
     # Download the html source to a temp file for quick parsing.
-    curl -s $url > $htmltemp
+    curl -s "$url" > $htmltemp
+    debug '$htmltemp = ' $htmltemp
     # ;exit is needed since sometimes data-title appears twice
-    folder_name=$(awk -F\" '/data-title/ {print $6; exit}' $htmltemp)
 
-    if [[ "$sanitize" == "TRUE" ]]
-    then # remove special characters
-      clean=${folder_name//_/} #turn / into _
-      clean=${clean// /_} #turn spaces into _
-      folder_name="${clean//[^a-zA-Z0-9_]/}" # remove all special chars
-    else
-      folder_name=$(sed 's/\//_/g' <<< $folder_name) # ensure no / chars
-    fi
-
-    if [[ "$preserve" == "TRUE" ]] || [ -z "$folder_name" ]
-    then # Create a name for a folder name based on the URL.
-      folder_name=$(basename "$url" | sed 's/\#.*//g')
-    fi
+    folder_name="$(parse_folder_name)"
 
     # It only takes one album named Pictures to possibly screw up
     # an entire folder. This will also save images to a new directory
     # if the script is used twice on the same album in the same folder.
     test -d "$folder_name" || folderexists="FALSE"
 
-    if [[ "$folderexists" == "TRUE" ]]
+    if [[ "$folderexists" == "TRUE" ]] 
     then
       tempdir=$(mktemp -d "$folder_name"_XXXXX) || exit 1
       folder_name="$tempdir"
     else
       mkdir -p "$folder_name"
     fi
+
+    debug '$folder_name = ' $folder_name
 
     # Save link to album in a text file with the images.
     echo "$url" >> "$folder_name"/"permalink.txt"
@@ -183,17 +199,20 @@ do
     do
       let total_images=$total_images+1
     done
+    debug '$total_images = ' $total_images
 
     # Iterate over all images found.
     for image_url in $(awk -F\" '/data-src/ {print $10}' $htmltemp | sed '/^$/d')
     do
-      # Some albums have the thumbnail images out of order, this fixes that.
-      data_index=$(grep $image_url $htmltemp | awk -F\" '{print $12}')
-      data_index=$(echo $data_index)
+      # Some albums have the thumbnail images out of order in the html source,
+      # this fixes that.
+      data_index="$(grep $image_url $htmltemp | awk -F\" '{print $12}')"
+      # data_index=$(echo $data_index)
       let data_index=$data_index+1
 
       # Ensure no images are thumbnails.
-      # Always works because all images that could be in $image_url are thumbnails.
+      # Always works because all images that could be in $image_url are currently
+      # thumbnails.
       image_url=$(sed 's/s.jpg/.jpg/g' <<< "$image_url")
 
       if [[ "$preserve" == "TRUE" ]]
@@ -248,12 +267,6 @@ do
     exit 1
   fi
 done
-
-if [[ "$silent_flag" == "FALSE" ]]
-then # Echo output
-  echo
-  echo "Finished with $count files downloaded."
-fi
 
 if [[ -s "$logfile" ]]
 then
